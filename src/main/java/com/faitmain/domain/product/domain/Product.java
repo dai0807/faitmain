d4952c3b90f913d9f702b0d826ee8716248e4cde
bitcamp <bitcamp@DESKTOP-KT7DNR2>
2022-05-31 10:52:12 +0900

    2022-05-31 손현지 Product 도메인 작성

---
+++ src/main/java/com/faitmain/domain/product/domain/Product.java
@@ -0,0 +1,32 @@
+package com.faitmain.domain.product.domain;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.faitmain.domain.user.domain.User;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Product {
+
+	private int productNumber;
+	private String productName;
+	private User store;
+	private int price;
+	private int productMainImage;
+	private List<String> productExtraImage;
+	private String productDetail;
+	private int productQuantity;
+	private String categoryCode;
+	private String productStatus;
+	private Date productRegDate;
+	private int deliveryCharge;
+	
+}
---
+++ src/test/java/com/faitmain/MySQLConnectionTest.java
@@ -0,0 +1,7 @@
+package com.faitmain;
+
+public class MySQLConnectionTest {
+
+	
+	
+}
