55c37f9b99fe2369829c0397ad3d8453e8c32ad8
bitcamp <bitcamp@DESKTOP-L03NVVH>
2022-05-31 20:03:59 +0900

    22-05-31 염현우 mapper 구현 중

---
+++ src/main/java/com/faitmain/domain/customer/domain/BanPeriod.java
@@ -0,0 +1,14 @@
+package com.faitmain.domain.customer.domain;
+
+import java.sql.Date;
+
+public class BanPeriod {
+	
+	private int report_number;
+	private String respondent_id;
+	private String respondent_nickname;
+	private String respondent_store_name;
+	private int status_number;
+	private int ban_period_number;
+	private Date ban_end_date;
+}
--- src/main/java/com/faitmain/domain/customer/domain/Customer.java
+++ src/main/java/com/faitmain/domain/customer/domain/Customer.java
@@ -1,4 +1,16 @@
 package com.faitmain.domain.customer.domain;
 
+import java.sql.Date;
+
 public class Customer{
+	
+	private int customer_board_number;
+	private String customer_board_title;
+	private String customer_board_content;
+	private Date reg_date;
+	private Date update_date;
+	private int customer_FAQ_category_code;
+	private char customer_board_type;
+	private String customer_id;
+	private int customer_count;
 }
