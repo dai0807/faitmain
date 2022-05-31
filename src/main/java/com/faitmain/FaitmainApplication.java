b04eca8caf1aa2ff7d2f89b741bad7cdf8bfd663
wumeenna <minu960604@gmail.com>
2022-05-31 09:00:34 +0900

    2022-05-31 Initial commit

---
+++ src/main/java/com/faitmain/FaitmainApplication.java
@@ -0,0 +1,13 @@
+package com.faitmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaitmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaitmainApplication.class, args);
+	}
+
+}
