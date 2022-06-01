62a06584e766a1ac5f37b856d48f6f778bc3a6f1
wumeenna <minu0604@naver.com>
2022-06-01 22:44:37 +0900

    2022-06-01 하민우 샘플용 member 생성

--- src/main/java/com/faitmain/domain/example/service/MemberService.java
+++ src/main/java/com/faitmain/domain/example/service/MemberService.java
@@ -1,19 +1,43 @@
 package com.faitmain.domain.example.service;
 
+import com.faitmain.domain.example.domain.Member;
 import com.faitmain.domain.example.mapper.MemberMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class MemberService{
-
-    @Autowired
     MemberMapper memberMapper;
+
+    public MemberService( MemberMapper memberMapper ){
+        this.memberMapper = memberMapper;
+    }
 
     public ArrayList< HashMap< String, Object > > findAll(){
         return memberMapper.findAll();
     }
+
+    public List< Member > getMemberList(){
+        return memberMapper.getMemberList();
+    }
+
+    public Member getMember( int id ){
+        return memberMapper.getMember( id );
+    }
+
+    public int createMember( Member member ){
+        return memberMapper.createMember( member );
+    }
+
+    public int updateMember( Member member ){
+        return memberMapper.updateMember( member );
+    }
+
+    public int deleteMember( int id ){
+        return memberMapper.deleteMember( id );
+    }
+
 }
