9ddb5c6329d43c6b9d338753a18750f32df98c9c
vcxzvxcz159 <vcxzvcxz159@gmail.com>
2022-06-01 11:45:05 +0900

    2022-06-01 전창현 LiveTest class 생성
    1. ControllerTest, MapperTest, ServiceTest 생성
    2. Search 필드 추가

--- src/main/java/com/faitmain/domain/model/Search.java
+++ src/main/java/com/faitmain/domain/model/Search.java
@@ -1,4 +1,11 @@
 package com.faitmain.domain.model;
 
+import lombok.Data;
+
+@Data
 public class Search{
+	private int currentPage;
+	private int pageSize;
+	private String searchCondition;
+	private String searchKeyword;
 }
