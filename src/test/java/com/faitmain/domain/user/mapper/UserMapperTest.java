771ec8aa25dd903ca30bdb1a54a97f7163d88bc5
dai3160@naver.com <dai3160@naver.com>
2022-06-02 02:59:41 +0900

    2022-06-02 양지안 User 빨간불 주석처리

--- src/test/java/com/faitmain/domain/user/mapper/UserMapperTest.java
+++ src/test/java/com/faitmain/domain/user/mapper/UserMapperTest.java
@@ -7,35 +7,43 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.faitmain.domain.product.domain.Product;
+import com.faitmain.domain.product.mapper.ProductMapperTest;
+import com.faitmain.domain.product.service.ProductService;
 import com.faitmain.domain.user.domain.User;
-import com.faitmain.domain.user.service.UserSerivce;
- 
+   
 @WebAppConfiguration
 @SpringBootTest
 public class UserMapperTest {
 
 	
-	
-    @Autowired
-	private UserSerivce userserivce;
-
-    @BeforeEach
-    public void setUp(){
-    }
-
-    @Test
-    @DisplayName( " 조회" )
-     
-    public void getUserTest() throws Exception{
-
-    	User user = userserivce.getUser("user01@naver.com");
-		System.out.println(user);
-		assertNotNull(user);
-    	
-    }
+//	
+//    @Autowired
+//	private UserSerivce userserivce;
+//
+//    @BeforeEach
+//    public void setUp(){
+//    }
+//
+//    @Test
+//    @DisplayName( " 조회" )
+//     
+//    public void getUserTest() throws Exception{
+//
+//    	User user = userserivce.getUser("user01@naver.com");
+//		System.out.println(user);
+//		assertNotNull(user);
+//    	
+//    }
 }
+//	Logger logger = LoggerFactory.getLogger(ProductMapperTest.class);
+
+ 
