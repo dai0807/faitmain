f217a8e36d2068f164f598377b18a04750a2b263
wumeenna <minu960604@gmail.com>
2022-06-03 09:15:21 +0900

    2022-05-31 하민우 order +++

--- src/main/java/com/faitmain/domain/order/service/OrderService.java
+++ src/main/java/com/faitmain/domain/order/service/OrderService.java
@@ -7,12 +7,15 @@ import java.util.Map;
 
 public interface OrderService{
 
+    //주문추가
     void addOrder( Order order ) throws Exception;
 
+    //주문상태업데이트
     int updateOrder( Order order ) throws Exception;
 
+    //주문확인
     Order getOrder( int OrderNumber ) throws Exception;
 
-
-    Map< String, Object > getOrderList( );
+    //주문조회
+    Map< String, Object > getOrderList( ) throws Exception;
 }
