cd8b8ec6fde9b0baf14d24c659d9a7aa46dbba33
vcxzvxcz159 <vcxzvcxz159@gmail.com>
2022-06-07 06:05:55 +0900

    2022-06-07 전창현 OpenApiRestController.java 생성
    1. token 받아오기 완성
    2. chatList 받아오기 완성
    3. chatRoom 생성 만드는 중

--- src/main/java/com/faitmain/domain/live/controller/LiveRestController.java
+++ src/main/java/com/faitmain/domain/live/controller/LiveRestController.java
@@ -1,36 +1,9 @@
 package com.faitmain.domain.live.controller;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.apache.ibatis.reflection.ExceptionUtil;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,75 +26,6 @@ public class LiveRestController {
 	// Field
 	@Autowired
 	private LiveService liveService;
-
-	@GetMapping("token")
-	public void getAccessToken() throws Exception {
-		log.info( "AccessToken = {} ", this.getClass() );
-		
-		 TrustManager[] trustCerts = new TrustManager[]{
-	                new X509TrustManager() {
-	                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-	                        return null;
-	                    }
-	                    public void checkClientTrusted(
-	                        java.security.cert.X509Certificate[] certs, String authType) {
-	                    }
-	                    public void checkServerTrusted(
-	                        java.security.cert.X509Certificate[] certs, String authType) {
-	                    }
-	                }
-	            };
-	 
-	         SSLContext sc = SSLContext.getInstance("TLSv1.2");
-	         sc.init(null, trustCerts, new java.security.SecureRandom());
-	         
-	         URL url = new URL("https://vchatcloud.com/openapi/token");
-	 
-	         HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
-	         conn.setSSLSocketFactory(sc.getSocketFactory());
-	         
-	         conn.setRequestMethod("GET");
-	         
-	         conn.setRequestProperty("Content-type", "application/json");
-	         conn.setRequestProperty("accept", "*/*");
-	         conn.setRequestProperty("api_key", "kmLueZ-chdq38-O7LGgP-Ggd14x-20220604144349");
-	         
-	         int responseCode = conn.getResponseCode();
-	         System.out.println("Sending 'GET' request to URL : " + url);
-	         System.out.println("Response Code : " + responseCode);
-	         
-	         Charset charset = Charset.forName("UTF-8");
-	         BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(),charset));
-	         String inputLine;
-	         StringBuffer response = new StringBuffer();
-	         while((inputLine = in.readLine()) != null) {
-	        	 response.append(inputLine);
-	         }
-	         in.close();
-	         
-	         System.out.println("response : " + response.toString());
-		
-//		String url = "https://vchatcloud.com/openapi/token";
-//		
-//		CloseableHttpClient httpClient = HttpClients.createDefault();
-//		
-//		HttpGet httpGet = new HttpGet(url);
-//		
-//		httpGet.addHeader("Content-type", "application/json");
-//		httpGet.addHeader("accept", "*");
-//		httpGet.addHeader("api_key", "kmLueZ-chdq38-O7LGgP-Ggd14x-20220604144349");
-//		
-//		CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
-//		
-//		System.out.println("Get Response Status");
-//		System.out.println(httpResponse.getStatusLine().getStatusCode());
-//		String json = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-//		
-//		System.out.println(json);
-//		
-//		httpClient.close();
-		 
-	}
 
 	// METHOD
 	// LIVE
@@ -207,5 +111,8 @@ public class LiveRestController {
 
 		return map;
 	}
+	
+	
+	
 
 }
