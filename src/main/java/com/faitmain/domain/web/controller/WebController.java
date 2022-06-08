66c6aede6d0a0d994482b95513919cd77518773d
vcxzvcxz159 <vcxzvcxz159@gmail.com>
2022-06-08 14:17:12 +0900

    2022-06-08 전창현 index.html 화면 구성

--- src/main/java/com/faitmain/domain/web/controller/WebController.java
+++ src/main/java/com/faitmain/domain/web/controller/WebController.java
@@ -2,17 +2,51 @@ package com.faitmain.domain.web.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.faitmain.domain.live.service.LiveService;
+import com.faitmain.domain.product.service.ProductService;
 
 @Slf4j
 @Controller
 @AllArgsConstructor
 public class WebController{
 
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+	
+	@Autowired
+	@Qualifier("liveServiceImpl")
+	private LiveService liveService;
+	
     @GetMapping( "/" )
-    public String main() throws Exception{
+    public String main(Model model) throws Exception{
         log.info( "log = {} " , this.getClass().getName() );
+        
+        Map<String, Object> map = new HashMap<String, Object>();
+        
+        map.put("orderName", "product_name DESC");
+		map.put("startRowNum", 1);
+		map.put("endRowNum", 5);
+		
+		map = productService.getProductList(map);
+        log.info("after getProductList");
+		
+		map.put("liveList", liveService.getLiveList().get("liveList"));
+		log.info("after getLiveList");
+		
+        System.out.println("map : " + map);
+        model.addAttribute("map", map);
+        
         return "index";
     }
 
