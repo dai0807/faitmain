b0dc4ca77402e8ce23cf67780dc1cec640de4f09
expertbronx <expertbronx@gmail.com>
2022-06-08 15:09:06 +0900

    2022-06-08 김현준 Live.html 수정 , liveRoomList.html, controller 작성

--- src/main/java/com/faitmain/domain/live/controller/LiveController.java
+++ src/main/java/com/faitmain/domain/live/controller/LiveController.java
@@ -1,10 +1,21 @@
 package com.faitmain.domain.live.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Map;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.servlet.http.HttpSession;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,6 +42,74 @@ public class LiveController {
    
    public LiveController() {
 	   log.info( "Controller = {} ", this.getClass() );
+   }
+   
+   @GetMapping( "liveRoom" )
+   public String getLiveRoomList( Model model ) throws Exception {
+      
+      System.out.println("/live/getLiveRoomList : GET start...");
+      log.info( "Controller = {} ", "/live/liveRoomList : GET start..." );
+      
+		log.info( "getRoomsList = {} ", this.getClass() );
+		
+		JSONObject result = null;
+		StringBuilder sb = new StringBuilder();
+		
+		 TrustManager[] trustCerts = new TrustManager[]{
+	                new X509TrustManager() {
+	                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+	                        return null;
+	                    }
+	                    public void checkClientTrusted(
+	                        java.security.cert.X509Certificate[] certs, String authType) {
+	                    }
+	                    public void checkServerTrusted(
+	                        java.security.cert.X509Certificate[] certs, String authType) {
+	                    }
+	                }
+	            };
+	 
+	         SSLContext sc = SSLContext.getInstance("TLSv1.2");
+	         sc.init(null, trustCerts, new java.security.SecureRandom());
+	         
+	         URL url = new URL("https://vchatcloud.com/openapi/v1/rooms");
+	 
+	         HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+	         conn.setSSLSocketFactory(sc.getSocketFactory());
+	         conn.setRequestMethod("GET");
+	         
+	         conn.setRequestProperty("Content-type", "application/json");
+	         conn.setRequestProperty("accept", "*/*");
+	         conn.setRequestProperty("api_key", "kmLueZ-chdq38-O7LGgP-Ggd14x-20220604144349");
+	         conn.setRequestProperty("X-AUTH-TOKEN", "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ2Y3h6dmN4ejE1OUBnbWFpbC5jb20iLCJleHAiOjE2NTQ2NzAxMDksImlhdCI6MTY1NDY1MjEwOSwiYXV0aG9yaXRpZXMiOiJbUk9MRV9VU0VSXSJ9.YHpIHG-4HyD-Iib4SRGjwxGkxsyaS5p69E5vLT46Elg");
+	         conn.setDoOutput(true);
+	         
+	         // 데이터 입력 스트림에 답기
+	         BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+	         while(br.ready()) {
+	        	 sb.append(br.readLine());
+	         }
+	         conn.disconnect();
+	         
+	         result = (JSONObject) new JSONParser().parse(sb.toString());
+	         
+	         // REST API 호출 상태 출력하기
+	         StringBuilder out = new StringBuilder();
+	         out.append(result.get("status") + " : " + result.get("status_message")+"\n");
+	         
+	         // JSON데이터에서 "data"라는 JSONObject를 가져온다.
+	         JSONArray data = (JSONArray)result.get("list");
+	         JSONObject tmp;
+	         for(int i=0; i<data.size(); i++) {
+	        	 tmp = (JSONObject)data.get(i);
+	        	 System.out.println("data["+i+"] : "+tmp);
+	         }  
+	         System.out.println("data : " + data);
+	         model.addAttribute("json", data);
+      
+	         	log.info( "Controller = {} ", "/live/liveRoomList : GET end..." );
+      
+      			return "view/live/liveList";
    }
    
    @GetMapping( "live" )
