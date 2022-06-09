5cea4523d8360a31d4f76fad4bdeb9d55db6ef97
손현지 <wiseson98@gmail.com>
2022-06-09 12:03:13 +0900

    1234

--- src/main/java/com/faitmain/domain/customer/mapper/CustomerMapper.java
+++ src/main/java/com/faitmain/domain/customer/mapper/CustomerMapper.java
@@ -1,50 +1,50 @@
-
-package com.faitmain.domain.customer.mapper;
-
-
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import com.faitmain.domain.customer.domain.BanStatus;
-import com.faitmain.domain.customer.domain.Customer;
-import com.faitmain.global.common.Image;
-
-@Mapper
-public interface CustomerMapper {
-
-	//CustomerBoard
-	
-	//INSERT 
-	public int addCustomerBoard(Customer customer) throws Exception;
-	
-	//INSERT
-	public void addCustomerBoardImage(Image image) throws Exception;
-	
-	//UPDATE
-	public int updateCustomerBoard(Customer customer) throws Exception;
-	
-	//UPDATE
-	public void updateCustomerBoardImage(Image image) throws Exception;
-	
-	//SELECT Detail
-	public Customer getCustomerBoard(int boardNumber) throws Exception;
-	
-	//SELECT
-	public Image getCustomerBoardImage(int boardNumber) throws Exception;
-	
-	//SELECT List
-	public List<Customer> getCustomerBoardList() throws Exception;
-	
-	//SELECT
-	public int deleteCustomerBoard(int boardNumber) throws Exception;
-	
-	//DELETE
-	public int DeleteCustomerBoard(Customer customer) throws Exception;
-	
-	
-
-	//BanStatus
-	public int updateBanStatus(BanStatus banStatus) throws Exception;
-}
-
+//
+//package com.faitmain.domain.customer.mapper;
+//
+//
+//import java.util.List;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//
+//import com.faitmain.domain.customer.domain.BanStatus;
+//import com.faitmain.domain.customer.domain.Customer;
+//import com.faitmain.global.common.Image;
+//
+//@Mapper
+//public interface CustomerMapper {
+//
+//	//CustomerBoard
+//	
+//	//INSERT 
+//	public int addCustomerBoard(Customer customer) throws Exception;
+//	
+//	//INSERT
+//	public void addCustomerBoardImage(Image image) throws Exception;
+//	
+//	//UPDATE
+//	public int updateCustomerBoard(Customer customer) throws Exception;
+//	
+//	//UPDATE
+//	public void updateCustomerBoardImage(Image image) throws Exception;
+//	
+//	//SELECT Detail
+//	public Customer getCustomerBoard(int boardNumber) throws Exception;
+//	
+//	//SELECT
+//	public Image getCustomerBoardImage(int boardNumber) throws Exception;
+//	
+//	//SELECT List
+//	public List<Customer> getCustomerBoardList() throws Exception;
+//	
+//	//SELECT
+//	public int deleteCustomerBoard(int boardNumber) throws Exception;
+//	
+//	//DELETE
+//	public int DeleteCustomerBoard(Customer customer) throws Exception;
+//	
+//	
+//
+//	//BanStatus
+//	public int updateBanStatus(BanStatus banStatus) throws Exception;
+//}
+//
