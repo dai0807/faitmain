7b8da0bbcea8fc86616538b3ded97f6545d675c3
wumeenna <minu0604@naver.com>
2022-06-11 18:38:32 +0900

    2022-06-11 하민우 CART ++++

--- src/main/java/com/faitmain/domain/product/controller/CartController.java
+++ src/main/java/com/faitmain/domain/product/controller/CartController.java
@@ -1,48 +1,50 @@
 package com.faitmain.domain.product.controller;
 
-import java.util.Map;
-
+import com.faitmain.domain.product.domain.Cart;
+import com.faitmain.domain.product.service.CartService;
+import com.faitmain.global.common.Search;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import com.faitmain.domain.product.service.CartService;
-import com.faitmain.global.common.Page;
-import com.faitmain.global.common.Search;
-
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
-@RequestMapping("/cart/")
+@RequestMapping( "/cart/" )
 @Slf4j
-public class CartController {
+public class CartController{
 
-	@Autowired
-	@Qualifier("cartServiceImpl")
-	private CartService cartService;
-	
-	public CartController() {
-		log.info("Controller = {} ", CartController.class);
-	}
-	
-	@GetMapping("getCartList")
-	public String getCartList(@ModelAttribute Search search, @RequestParam("userId") String userId, Model model) throws Exception {
-		
-		log.info("/cart/getCartList");
-		
-		if(search.getCurrentPage() == 0) {
-			search.setCurrentPage(1);
-		}
-		
-		search.setSearchKeyword(userId);
-		search.setPageSize(10);
-		
-		Map<String, Object> map = cartService.getCartList(search);
+    @Autowired
+    private CartService cartService;
+
+    public CartController(){
+        log.info( "Controller = {} " , CartController.class );
+    }
+
+    @PostMapping( "/cart/add" )
+    @ResponseBody
+    public String addCartPOST( Cart cart , HttpServletRequest request ){
+
+        /* 로그인 체크 */
+
+    }
+
+    @GetMapping( "getCartList" )
+    public String getCartList( @ModelAttribute Search search , @RequestParam( "userId" ) String userId , Model model ) throws Exception{
+
+        log.info( "/cart/getCartList" );
+
+        if ( search.getCurrentPage() == 0 ) {
+            search.setCurrentPage( 1 );
+        }
+
+        search.setSearchKeyword( userId );
+        search.setPageSize( 10 );
+
+        Map<String, Object> map = cartService.getCartList( search );
 /*				
 		Page resultPage = new Page( search.getCurrentPage(), ( Integer ) map.get( "totalCount" ) , 4, 10);
 		
@@ -51,8 +53,8 @@ public class CartController {
 		model.addAttribute("list", map.get("list"));
 		model.addAttribute("resultPage", resultPage);
 		model.addAttribute("search", search);
-*/		
-		return "forward:/cart/listCart.jsp";
-		
-	}
+*/
+        return "forward:/cart/listCart.jsp";
+
+    }
 }
