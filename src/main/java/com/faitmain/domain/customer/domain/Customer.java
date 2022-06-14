99a07968a978e1c641a1cc7bd5e80d3ed65544fd
bitcamp <bitcamp@DESKTOP-L03NVVH>
2022-06-14 20:03:40 +0900

    2022-06-14	염현우	등록, 조회, 수정, 삭제 기능 완료
    -- redirect 안됨 ..

--- src/main/java/com/faitmain/domain/customer/domain/Customer.java
+++ src/main/java/com/faitmain/domain/customer/domain/Customer.java
@@ -1,17 +1,10 @@
 package com.faitmain.domain.customer.domain;
 
-import java.sql.Date;
-import java.util.List;
-import java.util.Optional;
 
-import org.springframework.transaction.annotation.Transactional;
-
+import java.sql.Time;
+import java.sql.Timestamp;
 import com.faitmain.domain.user.domain.User;
-import com.faitmain.global.common.Image;
-
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 public class Customer{
@@ -19,17 +12,15 @@ public class Customer{
 	private int boardNumber;
 	private String boardTitle;
 	private String boardContent;
-//	private List<Image> boardImage;
-	private Date boardRegDate;
-	private Date boardUpdateDate;
+	private Timestamp boardRegDate;
+	private Time boardUpdateDate;
 	private int FAQCategoryCode;
 	private char boardType;
 	private User customerId;
 //	private int totalCount;
 	private int viewCount;
 	private String deleteYn;
-//	private String highlightNoticeYn;
-//	private Image image;
+
 
 
 	
