bdc9cf46342b3e3e1d4a292cbba23f60625ce604
bitcamp <bitcamp@DESKTOP-L03NVVH>
2022-06-15 20:19:31 +0900

    2022-06-15	염현우	등록, 조회, 수정, 삭제 완료 리다이렉트 완료
    ----
    썸머노트 구현 중(연동 완료)
    게시판 플래그 처리한거 어떻게 불러와야할지

--- src/main/java/com/faitmain/domain/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/faitmain/domain/customer/service/CustomerServiceImpl.java
@@ -6,34 +6,42 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.faitmain.domain.customer.domain.Customer;
 import com.faitmain.domain.customer.mapper.CustomerMapper;
 
+import lombok.RequiredArgsConstructor;
 
 
 
 
-@Service									//비즈니스 로직을 처리하는 서비스 클래스를 나타내는 어노테이션(해당 어노테이션 사용하여 스프링의 MVC의 서비스임을 나타냄)
+
+@Service("customerServiceImpl")
+@Transactional
 public class CustomerServiceImpl implements CustomerService{
 	
 	@Autowired
 	private CustomerMapper customerMapper;	//데이터베이스에 접근하는 DAO bean을 선언 
 	
-	@Override
-	public boolean registerCustomerBoard(Customer params) throws Exception {
-		int queryResult = 0;													//queryResult : Query를 실행한 횟수
-		
-		if(params.getBoardNumber() == 0) {									//'0' -> int default
-			queryResult = customerMapper.addCustomerBoard(params);
-		}else {
-			queryResult = customerMapper.updateCustomerBoard(params);
-		}
-		
-		return(queryResult == 1) ? true : false ;
-		
+	public CustomerServiceImpl(CustomerMapper customerMapper) {
+		this.customerMapper = customerMapper;
 	}
 	
+//	@Override
+//	public boolean registerCustomerBoard(Customer params) throws Exception {
+//		int queryResult = 0;													//queryResult : Query를 실행한 횟수
+//		
+//		if(params.getBoardNumber() == 0) {									//'0' -> int default
+//			queryResult = customerMapper.addCustomerBoard(params);
+//		}else {
+//			queryResult = customerMapper.updateCustomerBoard(params);
+//		}
+//		
+//		return(queryResult == 1) ? true : false ;
+//		
+//	}
+//	
 	@Override
 	public Customer getCustomerBoard(int boardNumber) throws Exception {
 		return customerMapper.getCustomerBoard(boardNumber);
@@ -41,18 +49,18 @@ public class CustomerServiceImpl implements CustomerService{
 	
 	
 
-//	@Override
-//	public int addCustomerBoard(Customer customer) throws Exception {
-//		customerMapper.addCustomerBoard(customer);
-//		return customerMapper.addCustomerBoard(customer);
-//		
-//	}
+	@Override
+	public int addCustomerBoard(Customer customer) throws Exception {
+		customerMapper.addCustomerBoard(customer);
+		return customerMapper.addCustomerBoard(customer);
+		
+	}
 	
-//	@Override
-//	public int updateCustomerBoard(Customer customer) throws Exception{ 
-//		 return customerMapper.updateCustomerBoard(customer);
-//	  
-//	}
+	@Override
+	public int updateCustomerBoard(Customer customer) throws Exception{ 
+		 return customerMapper.updateCustomerBoard(customer);
+	  
+	}
 
 	@Override
 	public boolean deleteCustomerBoard(int boardNumber) throws Exception {
