a58c88b1bfdbe16e51e7a686e74c1405e61f46e9
dai3160@naver.com <dai3160@naver.com>
2022-06-18 18:06:05 +0900

    2022 06 18 양지안

---
+++ src/main/java/com/faitmain/domain/user/domain/KakaoUserInfo.java
@@ -0,0 +1,10 @@
+package com.faitmain.domain.user.domain;
+
+import lombok.Data;
+
+@Data
+public class KakaoUserInfo   {
+    Long id;
+    String email;
+//출처: https://thalals.tistory.com/237 [힘차게, 열심히 공대생:티스토리]
+}
