2b08c12558cc2343d7fcc918cbeb59af7e3980a4
vcxzvcxz159 <vcxzvcxz159@gmail.com>
2022-06-20 16:32:53 +0900

    2022-06-20 전창현 +++

--- src/main/resources/static/js/liveReservationList.js
+++ src/main/resources/static/js/liveReservationList.js
@@ -1,30 +1,20 @@
-$(document).ready(function(){
+
 		  
-		  $(".carousel").carousel({
-		  
-		    interval: false,
-		    pause: true
-		  });
-		  
-		  $( ".carousel .carousel-inner" ).swipe( {
-		    swipeLeft: function ( event, direction, distance, duration, fingerCount ) {
-		      this.parent( ).carousel( 'next' );
+		  $( ".carousel" ).swipe({
+		    swipe: function ( event, direction, distance, duration, fingerCount ) {
+		      if(direction == 'left') $(this).carousel('next');
+		      if(direction == 'right') $(this).carousel('prev');
 		    },
-		    swipeRight: function ( ) {
-		      this.parent( ).carousel( 'prev' );
-		    },
-		    threshold: 0,
-		    tap: function(event, target) {
-		      // get the location: in my case the target is my link
-		      window.location = $(this).find('.carousel-item.active a').attr('href');
-		    },
-		   
-		    excludedElements:"label, button, input, select, textarea, .noSwipe"
-		  } );
+		    allowPageScroll:"vertical"
+		    
+		   //tap: function(event, target) {
+		   // get the location: in my case the target is my link
+		   //  window.location = $(this).find('.carousel-item.active a').attr('href');
+		   // },
 		  
-		  $('.carousel .carousel-inner').on('dragstart', 'a', function () {
-		    return false;
-		  });
+		  //$('.carousel .carousel-inner').on('dragstart', 'a', function () {
+		  //  return false;
+		  //});
 		  
 		  //$('a.btn-danger').on('click', function(){
 		  //	
@@ -37,5 +27,5 @@ $(document).ready(function(){
 		  //	})
 		  //	
 		  // });
+		});
 		  	  
-});
